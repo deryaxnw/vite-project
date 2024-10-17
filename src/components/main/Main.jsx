abeecd41d8783669fd6154ea1922217a35a11ea6
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-16 23:24:36 -0300

    main

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,23 @@
+import "./main.css"
+
+function Main(){
+    return (
+        <>
+           <main>
+                <h3>
+                    Home
+                </h3>
+
+                <div className="paragrafo">
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In alias molestias, ab deleniti et dolores veniam animi facilis soluta minima tempora quasi fugiat id omnis quibusdam maxime quo molestiae libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quisquam quibusdam culpa maiores distinctio voluptate tenetur? Cupiditate excepturi dolorem ad neque nesciunt repudiandae deleniti laboriosam in suscipit, voluptatem eos molestias. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio quaerat saepe officiis voluptatum ratione culpa beatae accusantium? Error soluta temporibus
+                    </p>
+                </div>
+
+            </main> 
+        
+        </>
+    );
+}   
+
+export default Main;
