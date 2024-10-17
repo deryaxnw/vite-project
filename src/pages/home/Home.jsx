32e7951f07f31013f3806a872c7b213eb56f14e8
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-16 23:23:42 -0300

    home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import Button from "../../components/button/Button";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Main from "../../components/main/Main";
+
+function Home(){
+    return (
+    <>
+    
+        <Header></Header>
+        <Main></Main>
+        <Button></Button>
+        <Footer></Footer>
+    </>
+    )
+}
+
+export default Home;
