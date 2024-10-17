a6ffd2a28d1444c15257c73e2220c8d2410799d4
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-17 11:57:03 -0300

    new-page-sobre

---
+++ src/pages/sobre/Sobre.jsx
@@ -0,0 +1,19 @@
+import "./sobre.css"
+import Header from "../../components/header/Header";
+import Texto from "../../components/sobre-nos/texto";
+import Footer from "../../components/footer/Footer";
+
+function Sobre(){
+return (
+    <>
+    <Header></Header>
+    <Texto></Texto>
+    <Footer></Footer>
+
+    {/* <Header></Header> */}
+    </>
+)
+
+}
+
+export default Sobre;
