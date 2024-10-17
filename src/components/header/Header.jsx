4e3fdab98c647d82ba552189b557120a6df91aa0
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-16 23:24:51 -0300

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import "./header.css"
+
+
+ function Header(){
+     return (
+        <>
+        <header>
+            <h1>Loja Boa</h1>
+            <nav>
+            <ul>
+                <li>Compras</li>
+                <li>Mercado</li>
+                <li>Vender</li>
+            </ul>
+            </nav>
+        </header>
+    </> 
+    );
+};
+
+export default Header;
