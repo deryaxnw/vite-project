c7f68a49974325400eb33edf77d18ea6849090ad
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-17 11:57:34 -0300

    texto - sobre

---
+++ src/components/sobre-nos/Texto.jsx
@@ -0,0 +1,23 @@
+import "./texto.css"
+
+function Texto(){
+    return (
+        <>
+        <div className="main">
+            <div className="texto">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem libero, harum iste, neque fuga vero accusantium, recusandae tenetur et perspiciatis officiis? Consectetur atque vitae natus? Eum explicabo corrupti rem sed?Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo in possimus neque vel incidunt aperiam ullam amet nam cum! Quo porro quis vero veritatis illo fuga dolor dolorem officia cum!</p>
+            </div>
+
+                <div className="image">
+                <img src="public/About us page-rafiki.svg" alt="" />
+
+                </div>
+            
+        </div>
+
+        
+        </>
+    )
+}
+
+export default Texto;
