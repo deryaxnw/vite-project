c345c7ff58891e71ae5b91437da2b6f1c80d49ce
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-17 15:17:40 -0300

    card da area produto

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,46 +15,46 @@ function Footer(){
 
                 <div id="footer_social_media">
 
-                    <a href="#" class="footer-link" id="instagram">
+                    <a href="#" className="footer-link" id="instagram">
                     <box-icon name='instagram' type='logo' ></box-icon>
                     </a>
 
-                    <a href="#" class="footer-link" id="whatsapp">
+                    <a href="#" className="footer-link" id="whatsapp">
                     <box-icon type='logo' name='whatsapp'></box-icon>
 
                     </a>
                 </div>
             </div>
         
-         <ul class="footer-list">
+         <ul className="footer-list">
 
             <li>
                 <h3>Blog</h3>
             </li>
             <li>
-                <a href="#" class="footer-link">Tech</a>
+                <a href="#" className="footer-link">Tech</a>
             </li>
             <li>
-                <a href="#" class="footer-link">Adventures</a>
+                <a href="#" className="footer-link">Adventures</a>
             </li>
             <li>
-                <a href="#" class="footer-link">Music</a>
+                <a href="#" className="footer-link">Music</a>
             </li>
          </ul>
 
-         <ul class="footer-list">
+         <ul className="footer-list">
 
             <li>
                 <h3>Products</h3>
             </li>
             <li>
-                <a href="#" class="footer-link">App</a>
+                <a href="#" className="footer-link">App</a>
             </li>
             <li>
-                <a href="#" class="footer-link">Desktop</a>
+                <a href="#" className="footer-link">Desktop</a>
             </li>
             <li>
-                <a href="#" class="footer-link">Cloud</a>
+                <a href="#" className="footer-link">Cloud</a>
             </li>
          </ul>
 
---
+++ src/pages/produtos/Produto.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/header/Header"
+import Carde from "../../components/card/Carde";
+import Footer from "../../components/footer/Footer";
+function Produto(){
+    return (
+        <>
+        <Header></Header>
+        <Carde></Carde>
+        <Footer></Footer>
+        </>
+    );
+}
+
+export default Produto;
